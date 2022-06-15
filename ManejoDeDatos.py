6ecb353a4554a2b17959f8efd43d14795c811a64
cva92 <carcecascante10@ole.augie.edu>
2022-06-15 15:53:59 -0600

    Creación de archivos para manejo de usuarios

--- ManejoDeDatos.py
+++ ManejoDeDatos.py
@@ -1,3 +1,21 @@
-def new_user(Name, Last_N, User, Password):
+def log_in(Usuario, Contraseña):
+    Users = open("LogIn_Data.txt", 'r')
+    Users_Data = load_scores_aux(Scores_F, 0, [], [])
+    Users.close()
+    return Matrix
+
+def load_scores_aux(File, i, Temp, Matrix):
+    if i == 7:
+        return Matrix
+    else:
+        Temp.append(File.readline()[:-1])
+        try:
+            Temp.append(int(File.readline()[:-1]))
+        except:
+            Temp.append(File.readline()[:-1])
+        Matrix.append(Temp)
+        return load_scores_aux(File, i+1, [], Matrix)
+
+
 
 
--- Usuarios.py
+++ Usuarios.py
@@ -1,2 +1,10 @@
-class Usuarios
-    def __init__(self,Name, Last_N, User, Password, email):
+class Usuarios:
+    def __init__(self,name, last_n, user, password, email):
+        self.name = name
+        self.last_n = last_n
+        self.user = user
+        self.password = password
+        self.email = email
+
+
+
